2cf1fa9a23f18b1394d095edb1aaff21f2bd820a
Brandon Hicks <tarellel@gmail.com>
2022-11-12 23:53:49 -0700

    CHANGED: Add target=_blank to project links

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   content: ['frontend/javascript/*.js','./output/**/*.html'],
+  // https://dev.to/andypeters/setup-bridgetown-to-use-tailwindcss-7nd
+  // content: ["./src/**/*.html", "./src/**/*.md", "./src/**/*.liquid", "./frontend/**/*.js", "./src/_data/**/*.yml"],
   // https://stackoverflow.com/questions/65554596/purgecss-and-tailwind-css-how-to-preserve-responsive-classes-using-the-command
   // defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   defaultExtractor: (content) => content.match(/[\w-/:.]+(?<!:)/g) || [],
