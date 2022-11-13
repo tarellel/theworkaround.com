fbf8e2affbf9d1809b41b40c562509deb30aec38
Brandon Hicks <tarellel@gmail.com>
2022-11-12 23:47:55 -0700

    CHANGED: Add tailwind specific extractor to purgeCSS

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   content: ['frontend/javascript/*.js','./output/**/*.html'],
+  // https://stackoverflow.com/questions/65554596/purgecss-and-tailwind-css-how-to-preserve-responsive-classes-using-the-command
+  // defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  defaultExtractor: (content) => content.match(/[\w-/:.]+(?<!:)/g) || [],
   output: "./output/_bridgetown/static"
 }
