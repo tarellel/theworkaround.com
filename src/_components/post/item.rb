8a3d0650af3311f417f21fb22af01d39109320c0
Brandon Hicks <tarellel@gmail.com>
2022-11-20 13:53:25 -0700

    CHANGED: Cleanup how the post lists are displayed

--- src/_components/post/item.rb
+++ src/_components/post/item.rb
@@ -7,5 +7,32 @@ module Post
     def initialize(post:)
       @post = post
     end
+
+    # Format the posts data to a US readable format
+    #
+    # @example:
+    #   Aug 27th, 2019
+    # @return [String]
+    def formatted_data
+      # post&.data&.date&.strftime('%b %d, %Y')
+      date_to_string(post&.data&.date, 'ordinal', 'US')
+    end
+
+    def post_length
+      return 0 unless post&.content
+
+      strip_html(post.content)&.split&.length
+    end
+
+    def post_word_txt
+      return 'word' if post_length == 1
+
+      'words'
+    end
+
+    # @return [String]
+    def post_tags
+      post.data.tags.map(&:capitalize).join(', ')
+    end
   end
 end
